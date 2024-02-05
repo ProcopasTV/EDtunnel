6695146ab5190b4026d38c1a598a63552b822e87
ProcopasTV <112422435+ProcopasTV@users.noreply.github.com>
2024-02-06 03:53:28 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '3232bbd8-b904-462d-a19a-6ae87e40f4d9';
 
-const proxyIPs = ['45.195.3.103'];
+const proxyIPs = ['156.253.7.110'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
